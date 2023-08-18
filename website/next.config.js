c622fe5c53bb74442d66518f1942a5b6908809e4
Uilian Comim <ucomim@hotmail.com>
2023-08-18 13:42:29 -0300

    refactor: updated .gitignore, added pkg lock files for version control

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/avatars/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/icons/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/embed/**",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
         fontFamily: {
             primary: ["Poppins", "Sans-serif"],
             secondary: ["Roboto", "Sans-serif"],
+        },
+        extend: {
+            gridTemplateColumns: {
+                "chat-footer": "1fr auto",
+            },
         },
     },
     plugins: [require("daisyui")],
